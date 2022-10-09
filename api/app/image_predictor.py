5fa3f0d8c76aa3ac3b55cad3b41a529580a4ea90
hasantavision <hasanta.vision@gmail.com>
2022-10-09 23:16:05 +0700

    fix bugs

--- api/app/image_predictor.py
+++ api/app/image_predictor.py
@@ -4,7 +4,7 @@ from PIL import Image
 class ImagePredictor:
 
     def init_ocr(self):
-        return easyocr.Reader(['id', 'en'])
+        return easyocr.Reader(['id', 'en'], model_storage_directory="/code/app/model")
 
     def predict_from_file(self, file_object):
         ocr = self.init_ocr()
